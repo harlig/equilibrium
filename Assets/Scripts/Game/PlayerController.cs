7271286f0b466605b5b6945e158cd1d7b64f4960
Ethan Harlig <ethan.harlig@gmail.com>
2023-11-04 00:08:35 -0700

    players can have hp

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,12 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.UI;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshPro hpTextElement;
+    private float hpRemaining;
+    private const int MAX_HP = 100;
     private const float MOVEMENT_SPEED = 0.1f;
+
+    void Awake()
+    {
+        hpRemaining = MAX_HP;
+        hpTextElement.text = $"{hpRemaining}";
+    }
 
     void FixedUpdate()
     {
