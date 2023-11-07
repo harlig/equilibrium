bcb635aa0564f58caddb82c7bbee6a20924cc8d7
Ethan Harlig <ethan.harlig@gmail.com>
2023-11-06 23:24:19 -0800

    remove dead code

--- Assets/Scripts/Game/Levels/RoomManager.cs
+++ Assets/Scripts/Game/Levels/RoomManager.cs
@@ -14,23 +14,15 @@ public class RoomManager : MonoBehaviour
     void Start()
     {
         CalculateGridDimensions();
-        Debug.Log($"room size: ({minX},{minY}, ({maxX}, {maxY})");
     }
 
     void CalculateGridDimensions()
     {
-        // Get the bounds of the wall Tilemap.
         BoundsInt bounds = wallTilemap.cellBounds;
 
         Vector3 minWorld = wallTilemap.GetCellCenterWorld(bounds.min);
         Vector3 maxWorld = wallTilemap.GetCellCenterWorld(bounds.max);
 
-        // // Convert the world positions to local positions relative to the root object.
-        // Vector3 minLocal = transform.InverseTransformPoint(minWorld);
-        // Vector3 maxLocal = transform.InverseTransformPoint(maxWorld);
-        // Debug.Log($"minLocal: {minLocal}; maxLocal: {maxLocal}");
-
-        // Update the minX, minY, maxX, and maxY.
         minX = Mathf.FloorToInt(minWorld.x);
         minY = Mathf.FloorToInt(minWorld.y);
         maxX = Mathf.CeilToInt(maxWorld.x);
