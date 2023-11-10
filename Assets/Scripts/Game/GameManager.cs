587b9fba8f8b04099be5149fe08008837cad5500
Ethan Harlig <ethan.harlig@gmail.com>
2023-11-10 14:36:43 -0800

    add more levels

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,5 +4,5 @@ public class GameManager
 {
     // if you are at level 0, you need 1 xp to level up. if you are at level 1, you need 10, etc.
     public static List<int> XpNeededForLevelUpAtIndex { get; } =
-        new() { 1, 2, 3, 4, 5, 11, 21, 31, 41 };
+        new() { 1, 2, 3, 4, 5, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101 };
 }
