c4e4110f545dbcf77569213ec043753ec5a46a8b
Ethan Harlig <ethan.harlig@gmail.com>
2023-11-14 22:33:55 -0800

    add more enemies to level 2

--- Assets/Scripts/Game/Levels/Floors/SecondFloor.cs
+++ Assets/Scripts/Game/Levels/Floors/SecondFloor.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 
 public class SecondFloor : FloorManager
 {
-    public override List<Vector2> MeleeEnemySpawnLocations => new() { new(2, 2) };
+    public override List<Vector2> MeleeEnemySpawnLocations =>
+        new() { new(2, 2), new(18, 3), new(2, 18), new(10, 18) };
 }
