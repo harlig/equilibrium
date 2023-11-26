40142915382a1f2232852aa47a31b586b6821c2f
Ethan Harlig <ethan.harlig@gmail.com>
2023-11-25 23:11:21 -0800

    adjust melee weapon hitbox

--- Assets/Scripts/Game/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Game/Weapons/MeleeWeapon.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class MeleeWeapon : WeaponController
 {
-    // private bool isSwinging = false;
     private WeaponAnimator weaponAnimator;
     public FirestarterSystem firestarterSystem = new();
 
@@ -31,34 +30,6 @@ public class MeleeWeapon : WeaponController
     {
         // TODO: apply animations and make the weapon degree dynamic
         weaponAnimator.DoSwing(position);
-        // StartCoroutine(DoStrikeAnimation(0.1f, position));
-    }
-
-    private IEnumerator DoStrikeAnimation(float waitTime, Vector2 position)
-    {
-        // strike with the weapon (crude animation)
-        var originalRot = transform.rotation;
-        var originalPos = transform.position;
-        RotateByDegrees(90, position);
-
-        yield return new WaitForSeconds(waitTime);
-        // return to original position
-        transform.rotation = originalRot;
-        transform.position = originalPos;
-    }
-
-    private void RotateByDegrees(float degrees, Vector2 pivotPoint)
-    {
-        Vector3 pivotPoint3d = new(pivotPoint.x, pivotPoint.y, 0f);
-        Vector3 objectPosition = transform.position;
-
-        // calculate the rotated position of the object
-        Vector3 rotatedPos =
-            Quaternion.Euler(0, 0, degrees) * (objectPosition - pivotPoint3d) + pivotPoint3d;
-
-        // apply the new position and rotate the weapon
-        transform.position = rotatedPos;
-        transform.rotation = Quaternion.Euler(0, 0, degrees);
     }
 
     private void ApplyCharacterDamage(CharacterController character, float damageModifier)
