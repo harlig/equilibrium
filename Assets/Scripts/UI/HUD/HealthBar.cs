ab8bea21b1fe02caf7ac4ac571a96003391df02b
Ethan Harlig <ethan.harlig@gmail.com>
2023-11-30 01:25:35 -0800

    fix race condition

--- Assets/Scripts/UI/HUD/HealthBar.cs
+++ Assets/Scripts/UI/HUD/HealthBar.cs
@@ -3,22 +3,21 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     public RectTransform hpBar;
-    private float originalAnchorMaxX;
+    private float? originalAnchorMaxX;
 
     void Start()
     {
-        // Save the original right-side anchor position
-        originalAnchorMaxX = hpBar.anchorMax.x;
         SetHealth(1);
     }
 
     public void SetHealth(float healthPercent)
     {
+        originalAnchorMaxX ??= hpBar.anchorMax.x;
         // Clamp the health percentage to ensure it's between 0 and 1
         healthPercent = Mathf.Clamp01(healthPercent);
 
         // Set the new right-side anchor position based on the health percentage
-        hpBar.anchorMax = new Vector2(originalAnchorMaxX * healthPercent, hpBar.anchorMax.y);
+        hpBar.anchorMax = new Vector2(originalAnchorMaxX.Value * healthPercent, hpBar.anchorMax.y);
 
         // Optionally, adjust the anchored position if needed
         // hpBar.anchoredPosition = new Vector2(calculatedXPosition, hpBar.anchoredPosition.y);
--- Assets/Scripts/UI/HUD/XpBar.cs
+++ Assets/Scripts/UI/HUD/XpBar.cs
@@ -10,20 +10,19 @@ public class XpBar : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI levelText;
-    private float originalAnchorMaxY;
+    private float? originalAnchorMaxY = null;
 
     void Start()
     {
-        // Save the original top-side anchor position
-        originalAnchorMaxY = xpBar.anchorMax.y;
         SetPercentUntilLevel(0, 0);
         SetLevelText(0);
     }
 
     public void SetPercentUntilLevel(float xpPercent, float totalXpCollected)
     {
+        originalAnchorMaxY ??= xpBar.anchorMax.y;
         xpPercent = Mathf.Clamp01(xpPercent);
-        xpBar.anchorMax = new Vector2(xpBar.anchorMax.x, originalAnchorMaxY * xpPercent);
+        xpBar.anchorMax = new Vector2(xpBar.anchorMax.x, originalAnchorMaxY.Value * xpPercent);
         xpText.text = $"{string.Format("{0:N0}", totalXpCollected)}xp";
     }
 
