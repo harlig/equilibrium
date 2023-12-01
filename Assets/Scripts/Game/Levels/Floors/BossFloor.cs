241fb46ab056ed1128396822521974c1c53dde6a
Ethan Harlig <ethan.harlig@gmail.com>
2023-12-01 12:45:13 -0800

    more bosses

--- Assets/Scripts/Game/Levels/Floors/BossFloor.cs
+++ Assets/Scripts/Game/Levels/Floors/BossFloor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BossFloor : FloorManager
 {
     public override List<EnemyConfiguration> EnemySpawnLocations =>
-        new() { EnemyConfiguration.Create(0, 0, 2) };
+        new() { EnemyConfiguration.Create(0, 0, 5) };
 
     void Awake()
     {
