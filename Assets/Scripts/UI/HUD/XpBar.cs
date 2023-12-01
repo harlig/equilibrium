00623ac7708001001b3896cfc2ed929b3456c469
Ethan Harlig <ethan.harlig@gmail.com>
2023-12-01 00:44:46 -0800

    xp bar stays maxed at max level

--- Assets/Scripts/UI/HUD/XpBar.cs
+++ Assets/Scripts/UI/HUD/XpBar.cs
@@ -12,6 +12,8 @@ public class XpBar : MonoBehaviour
     private TextMeshProUGUI levelText;
     private float? originalAnchorMaxY = null;
 
+    private int playerLevel;
+
     void Start()
     {
         SetPercentUntilLevel(0, 0);
@@ -21,13 +23,21 @@ public class XpBar : MonoBehaviour
     public void SetPercentUntilLevel(float xpPercent, float totalXpCollected)
     {
         originalAnchorMaxY ??= xpBar.anchorMax.y;
+
         xpPercent = Mathf.Clamp01(xpPercent);
+        if (playerLevel >= GameManager.XpNeededForLevelUpAtIndex.Count)
+        {
+            xpPercent = 1;
+        }
+
         xpBar.anchorMax = new Vector2(xpBar.anchorMax.x, originalAnchorMaxY.Value * xpPercent);
         xpText.text = $"{string.Format("{0:N0}", totalXpCollected)}xp";
     }
 
     public void SetLevelText(int newLevel)
     {
+        playerLevel = newLevel;
+
         levelText.text = $"{newLevel}";
     }
 }
